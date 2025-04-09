65734466f6e4ba72648c17740c51a6b07a97069f
ellafenlon3 <ellafenlon3@gmail.com>
2025-04-09 18:01:27 +0000

    ggyttt

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Subscription Analyzer", layout="centered")
 
-st.title("📊 Student Subscription Analyzer")
+st.title("📊 SubTracker")
 
 
 # Read the uploaded Excel file
